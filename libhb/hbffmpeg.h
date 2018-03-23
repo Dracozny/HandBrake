51a588e6147d14387aec6a14db1666f5b6f60abd
Dracozny <dracozny@gmail.com>
2018-03-22 19:24:45 -0700

    Revert "Ffmpeg"

--- libhb/hbffmpeg.h
+++ libhb/hbffmpeg.h
@@ -24,7 +24,7 @@
 
 void hb_avcodec_init(void);
 int  hb_avcodec_open(AVCodecContext *, AVCodec *, AVDictionary **, int);
-void hb_avcodec_free_context(AVCodecContext **avctx);
+int  hb_avcodec_close(AVCodecContext *);
 const char* const* hb_av_preset_get_names(int encoder);
 
 uint64_t hb_ff_mixdown_xlat(int hb_mixdown, int *downmix_mode);
